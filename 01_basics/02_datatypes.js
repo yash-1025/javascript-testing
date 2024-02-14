acf88063f5f3e38940c8df400a834d169a829f40
yash-1025 <120320005+yash-1025@users.noreply.github.com>
2024-02-14 18:34:05 +0000

    datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -3,4 +3,20 @@
 // alert(3 + 3) // we are using nodejs, not browser
 
 console.log(3+3)
+//code readabiblity high honi chahiye
 
+let name = "yash"
+let age = 20
+let isLoggedIn = true
+let state = null
+// number => 2 to the power of 53
+// bigint
+// string => ""
+// boolean 
+// null => is a stand alone value 
+// undefined
+// symbol => unique
+
+// object 
+
+console.log(typeof null)//object
