cc20f06cb7cc75ff00f83fdfd9f7eff4737d0dba
yash-1025 <120320005+yash-1025@users.noreply.github.com>
2024-02-15 17:05:01 +0000

    comparisons

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,10 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 2);
+// console.log(2 != 1);
+
+// equality check and comparision operators work differently
+
+// === checks value and datatype as well
+
