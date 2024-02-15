206cb02700606fd40a8f6aa32791d86c49680f4d
yash-1025 <120320005+yash-1025@users.noreply.github.com>
2024-02-15 16:38:45 +0000

    postfix and prefix

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -13,8 +13,8 @@ let valueInNumber = Number(score)
 
 let isLoggedIn = "yash"
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(typeof booleanIsLoggedIn)
-console.log(booleanIsLoggedIn);
+//console.log(typeof booleanIsLoggedIn)
+//console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
@@ -23,4 +23,26 @@ console.log(booleanIsLoggedIn);
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
+//console.log(typeof stringNumber);
+
+//operations
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+let str1 = "hello"
+let str2 = "yash"
+
+let str3 = str1 + str2
+//console.log(str3);
+
+//console.log("1" + 2);
+console.log(+true);
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
