acc7f8f66c75fa00b1a130f9c57dde516859f054
yash-1025 <120320005+yash-1025@users.noreply.github.com>
2024-02-25 19:50:32 +0000

    datatype summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,21 @@
+// primitive data types
+
+// 7 categories 
+// String, number, boolean, null, undefinfed, symbol, BigInt
+
+const score = false
+const scoreValue = 100.3
+// reference type or non primitive
+
+const heros = ["shaktiman", "naagraj", "doga"];
+
+// curly braces mei objects hote hai
+let myObj = {
+    name: "yash",
+    age: 22,
+}
+// array, objects, functions
+
+const myFuntion = function(){
+    console.log("Hello World");
+}
